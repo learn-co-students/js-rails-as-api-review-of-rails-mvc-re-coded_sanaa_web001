c41f9bff09ee38415db7d067669edcdc0d94dfb7
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-23 18:31:34 +0000

    Done.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,1 +1,13 @@
 # Add code from Readme
+class Bird < ApplicationRecord
+end
+
+Rails.application.routes.draw do
+  get '/birds' => 'birds#index'
+end
+
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+  end
+end
